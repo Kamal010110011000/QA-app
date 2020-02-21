1f3285a4c26a2e7d2d5879f860cf12a5e082e1f3
Kamal010110011000 <kamalsingh.4iv1@gmail.com>
2020-02-21 22:56:28 +0530

    Add Question Form

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -16,13 +16,24 @@
 
                 <div class="card-body">
                     <form action="{{ route('question.store') }}" method="post">
+                        @csrf
                         <div class="form-group">
                             <label for="question-title">Question Title</label>
-                            <input type="text" name="title" id="question-title" class="form-control">
+                            <input type="text" name="title" id="question-title" class="form-control {{$errors->has('title') ? 'is-invalid' : '' }}">
+                            @if ($errors->has('title'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('title') }}</strong>
+                                </div>
+                            @endif
                         </div>
                         <div class="form-group">
                             <label for="question-body">Explain Your Question</label>
-                            <textarea name="body" id="question-body" rows="10" class="form-control"></textarea>
+                            <textarea name="body" id="question-body" rows="10" class="form-control {{$errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+                            @if ($errors->has('body'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('body') }}</strong>
+                                </div>
+                            @endif
                         </div>
                         <div class="form-group">
                             <button class="btn-outline-primary btn btn-lg"> Ask Questions</button>
